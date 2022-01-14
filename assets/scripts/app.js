59d73639e402c64ee80d6c0122a3c597dd742197
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-01-14 12:44:35 -0500

    USe handler

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,13 +2,23 @@ let maxMonsterLife = 100;
 let maxPlayerLife = 100;
 let currentMonsterLife = maxMonsterLife;
 let currentPlayerLife = maxPlayerLife;
+
 const ATTACK_VAL = 10;
+const MONSTER_ATTACK_VAL = 14;
 
 adjustHealthBars(maxMonsterLife);
 
 function attackHandler() {
-    const damageDealt = dealMonsterDamage(ATTACK_VAL);
-    currentMonsterLife -= damageDealt;
+    const damageMonster = dealMonsterDamage(ATTACK_VAL);
+    currentMonsterLife -= damageMonster;
+    const damagePlayer = dealPlayerDamage(MONSTER_ATTACK_VAL);
+    currentPlayerLife -= damagePlayer;
+    if (currentMonsterLife <= 0 && currentPlayerLife > 0) {
+        alert("You won!");
+    } else if (currentPlayerLife <= 0) {
+        alert("You lost!");
+    } else {
+    }
 }
 
 attackBtn.addEventListener("click", attackHandler);
